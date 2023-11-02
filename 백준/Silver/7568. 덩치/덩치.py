70f060fd58439f2161ef3d45beee8f99382b478c
zisu <69131324+seo101500@users.noreply.github.com>
2023-11-02 15:50:28 +0900

    Update 덩치.py

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,13 +1,13 @@
-N = int(input()) #전체 사람 수
-people = [] #사람 정보를 받을 list
+n = int(input())
+data = []
 
-for _ in range(N): #입력한 순서대로 정보를 입력받는다.
+for i in range(n):
     x, y = map(int, input().split())
-    people.append((x,y))
+    data.append((x,y))
 
-for i in people:
-    rank = 1 #초기값은 전부 1
-    for j in people:
-        if i[0] < j[0] and i[1] < j[1]: #조건
-            rank+=1 #조건 만족 시 count up
-    print(rank, end = " ") #바로 출력
+for j in data:
+    rank = 1 
+    for z in data:
+        if j[0] < z[0] and j[1] < z[1]:
+            rank+=1 
+    print(rank, end = " ")
